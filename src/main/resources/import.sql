4db33e259a9b04c2627a2c7fd2ced897db38e673
Nagaraj <madinagaraj@gmail.com>
2021-08-09 00:11:15 -0700

    Swagger Configuration update

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO ASSIGNMENT VALUES(1, 'Assignment for Maths', '101','Daily','Maths','Maths','Credit')
+INSERT INTO ASSIGNMENT VALUES(2, 'Assignment for Science', '102','Daily','Science','Science','Credit')
+INSERT INTO ASSIGNMENT VALUES(3, 'Assignment for English', '103','Daily','English','English','Credit')
+INSERT INTO ASSIGNMENT VALUES(4, 'Assignment for Kannada', '104','Weekly','Kannada','Kannada','Add On')
